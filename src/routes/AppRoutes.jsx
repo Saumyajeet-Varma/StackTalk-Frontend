f3a584ef6acc595de1a281496e88ac9285916dd8
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-05-04 12:07:22 +0530

    login and register UI created

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Homepage from "../pages/Homepage"
+import Login from "../pages/Login"
+import Register from "../pages/Register"
 
 const AppRoutes = () => {
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<div>Home</div>} />
-                <Route path="/login" element={<div>Login</div>} />
-                <Route path="/register" element={<div>Register</div>} />
+                <Route path="/" element={<Homepage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="*" element={<div>Error</div>} />
             </Routes>
         </BrowserRouter>
