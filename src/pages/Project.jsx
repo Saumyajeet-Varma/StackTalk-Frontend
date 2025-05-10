ecfcd416b165475a079b6844b1a66ea3bb5c5d31
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-05-10 12:12:24 +0530

    chat sidebar done

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react"
 import { useLocation } from "react-router-dom"
 
 const Project = () => {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
     const location = useLocation()
 
@@ -9,13 +12,13 @@ const Project = () => {
     return (
         <main className="h-screen w-screen flex">
 
-            <section className="left h-full min-w-72 bg-slate-200 w-1/4 flex flex-col">
+            <section className="left relative h-full min-w-72 bg-slate-200 w-1/4 flex flex-col">
                 <div className="chat-header px-4 py-2 flex justify-between w-full items-center bg-slate-300">
                     <button className='flex gap-2'>
                         <i className="ri-add-fill"></i>
                         <p>Add collaborator</p>
                     </button>
-                    <button className='p-2'>
+                    <button className='p-2' onClick={() => setIsSidebarOpen(true)}>
                         <i className="ri-group-fill"></i>
                     </button>
                 </div>
@@ -41,7 +44,28 @@ const Project = () => {
                     </div>
                 </div>
 
-                <div className="side-panel"></div>
+                <div className={`sidebar bg-slate-200 h-full w-full absolute transition-all ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} top-0`}>
+                    <div className="sidebar-header flex justify-end px-4 py-2 bg-slate-300">
+                        <button className='p-2' onClick={() => setIsSidebarOpen(false)}>
+                            <i className="ri-close-fill text-lg"></i>
+                        </button>
+                    </div>
+
+                    <div className="collaborators flex flex-col gap-1 items-center py-1">
+                        <div className="collaborator cursor-pointer hover:bg-slate-300 w-full p-2 flex gap-2 items-center">
+                            <div className="aspect-square rounded-full w-fit h-fit flex items-center justify-center p-2 text-white bg-slate-600">
+                                <i className="ri-user-fill text-2xl"></i>
+                            </div>
+                            <h1 className="font-semibold text-lg">username00</h1>
+                        </div>
+                        <div className="collaborator cursor-pointer hover:bg-slate-300 w-full p-2 flex gap-2 items-center">
+                            <div className="aspect-square rounded-full w-fit h-fit flex items-center justify-center p-2 text-white bg-slate-600">
+                                <i className="ri-user-fill text-2xl"></i>
+                            </div>
+                            <h1 className="font-semibold text-lg">username00</h1>
+                        </div>
+                    </div>
+                </div>
             </section>
 
             {/* <section className="right"></section> */}
